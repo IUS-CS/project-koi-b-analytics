94a1eb1062348d5b86e1c0864ba59af7a6534c73
brimstim <ibrimner@iu.edu>
2021-04-12 11:50:01 -0400

    added dropdown and carousel functionailty for season selection

--- src/koib_build.py
+++ src/koib_build.py
@@ -4,12 +4,16 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.image import Image
+#from kivy.uix.dropdown import DropDown
+#from kivy.base import runTouchApp
 kivy.require('1.9.0')
 from kivy.config import Config
 Config.set('graphics', 'width', '1920')
 Config.set('graphics', 'height', '1080')
 from kivy.core.window import Window
-
+#import psycopg2
+from sqlalchemy import create_engine
+engine = create_engine("postgresql://doadmin:show-password@db-postgresql-nyc3-35484-do-user-8902057-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require")
 
 
 
@@ -17,27 +21,14 @@ from kivy.core.window import Window
 Builder.load_file('my.kv')
 
 class MyLayout(Widget):
-    pass
-    #name = ObjectProperty(None)
-    #email = ObjectProperty(None)
-    #phone = ObjectProperty(None)
-    #press fn
-    #def press(self):
-    #    name = self.name.text
-    #    phone = self.phone.text
-    #    email = self.email.text
-        #self.add_widget(Label(text=f'Hello {name}, is {phone} your number and {email} your email?'))
-    #    print(f'Hello {name}, is {phone} your number and {email} your email?')
-    #    #clear input feilds
-    #    self.name.text = ''
-    #    self.phone.text = ''
-    #    self.email.text = ''
+    def press(self):
+        self.ids.buildR.outline_color = utils.get_color_from_hex('#86c232')
 
 #MAIN CLASS
 #my.kv file used for MyApp
 class MyApp(App):
     def build(self):
-        Window.clearcolor = (71/255.0,75/255.0,79/255.0,1)
+        Window.clearcolor = (40/255.0,44/255.0,52/255.0,1)
         return MyLayout()
 
 if __name__ == '__main__':
