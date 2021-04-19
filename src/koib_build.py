3be0908a4907cd157682f40fdcd011accfccc8f3
brimstim <brimner.isaac@gmail.com>
2021-04-19 14:47:00 -0400

    getting looking purdy

--- src/koib_build.py
+++ src/koib_build.py
@@ -17,6 +17,7 @@ import psycopg2
 from sqlalchemy import create_engine
 import pandas as pd
 from pandasgui import show
+import numpy as np
 #CONNECTING TO HOSTED POSTGRES DATABASE___ WE MADE THIS WAY HARDER THAN IT NEEDED TO BE AND IT ATE UP WAY TOO MUCH TIME. BUT HEY IT WORKS NOW
 engine = create_engine("postgresql://doadmin:a8kz5iiddjdx1fdm@db-postgresql-nyc3-35484-do-user-8902057-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require")
 connection = engine.connect()
@@ -39,6 +40,27 @@ class SecondWindow(Screen):
 #button functions are in order as they appear in GUI
     def clear_input(self):#CLEAR USER INPUT
         self.ids.user_input.text = ""
+    def srch_btn(self):#UPDATES PRELIMINARY STATS SO USER CAN DECIDE IF THEY WANT TO ADD THIS PLAYER
+        chosen_player = str(self.ids.user_input.text)
+        #line below grabs stats for updating label
+        query = pd.read_sql_query("select \"Player\", \"Tm\", \"Pos\", \"PTS\", \"FG%\" from t20_21_pergame WHERE lower(\"Player\") LIKE lower('%"+chosen_player+"%');", conn)
+        #This keeps the program from crashing if the query returns an empty dataframe (2 lines below)
+        if query.empty == True:
+            return
+        #creating variables that reference certain fields within the stats table
+        global player_name
+        player_name = query.loc[0].at["Player"]
+        team_name = query.loc[0].at["Tm"]
+        pos_name = query.loc[0].at["Pos"]
+        pts_name = query.loc[0].at["PTS"]
+        fg_name = query.loc[0].at["FG%"]
+        print(query)
+        #updating preliminary stat labels based on user input and clicking of search button
+        self.ids.stat_name.text = "NAME: " + player_name
+        self.ids.stat_team.text = "TEAM: " + team_name
+        self.ids.stat_pos.text = "POSITION: " + pos_name
+        self.ids.stat_pts.text = "PPG: " + pts_name
+        self.ids.stat_fg.text = "FG %: " + fg_name
     def clear_roster(self):#CLEAR ROSTER
         self.ids.player1.text = "PLAYER 1"
         self.ids.player2.text = "PLAYER 2"
@@ -47,13 +69,33 @@ class SecondWindow(Screen):
         self.ids.player5.text = "PLAYER 5"
         myroster.clear() #this removes players from roster
     def add_to_roster(self):#THIS FN UPDATES myroster AND RENAMES LABEL TEXT IN GUI TO REFLECT THIS CHANGE
-        chosen_player = str(self.ids.user_input.text)
-        query = pd.read_sql_query("SELECT * FROM t20_21_pergame", conn)
-        #query = pd.read_sql_query('SELECT * FROM "t20_21_pergame" WHERE "Player" LIKE '%LeBron%'', conn)
-        #query = pd.read_sql_query("select "Player", "Tm", "PTS", "FG%", "3P%" from t20_21_pergame WHERE lower("Player") LIKE lower('%lebron%');", conn)
-        #test = self.ids.user_input.text
+        self.ids.user_input.text = ""
+        roster_length = len(myroster)
+        if roster_length == 5:#this keeps the roster list at a max of 5 & adds the final 5 spot if it is the last player to be added
+            self.ids.player5.text  = player_name
+            myroster.append(player_name)
+            return
+        myroster.append(player_name)# adding players to myroster list
+        roster_length = len(myroster)
+        print(myroster)#testing purposes
+        #iterating through list to update labels
+        for i in myroster:
+            if roster_length == 1:
+                self.ids.player1.text = player_name
+            if roster_length == 2:
+                self.ids.player2.text  = player_name
+            if roster_length == 3:
+                self.ids.player3.text  = player_name
+            if roster_length == 4:
+                self.ids.player4.text  = player_name
+    def anal_roster(self):
+        p1 = myroster[0]
+        p2 = myroster[1]
+        p3 = myroster[2]
+        p4 = myroster[3]
+        p5 = myroster[4]
+        query = pd.read_sql_query("select * from t20_21_pergame WHERE \"Player\" = '"+p1+"' OR \"Player\" = '"+p2+"' OR \"Player\" = '"+p3+"' OR \"Player\" ='"+p4+"' OR \"Player\" ='"+p5+"';", conn)
         show(query)
-        #print(query)
 class WindowManager(ScreenManager):
     pass
 
