c5c344e1c7eef99c785ccfbbdf29dc9edb394dcd
brimstim <ibrimner@iu.edu>
2021-04-20 18:24:08 -0400

    it is done fellas. We did it.

--- src/koib_build.py
+++ src/koib_build.py
@@ -6,13 +6,15 @@ from kivy.uix.dropdown import DropDown
 from kivy.base import runTouchApp
 from kivy.lang import Builder
 from kivy.uix.image import Image
+from kivy.animation import Animation, AnimationTransition
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 kivy.require('1.9.0')
-Config.set('graphics', 'width', '1920')
-Config.set('graphics', 'height', '1080')
-
+Config.set('graphics', 'width', '1280')
+Config.set('graphics', 'height', '720')
+#Config.write()
 import psycopg2
 from sqlalchemy import create_engine
 import pandas as pd
@@ -56,11 +58,11 @@ class SecondWindow(Screen):
         fg_name = query.loc[0].at["FG%"]
         print(query)
         #updating preliminary stat labels based on user input and clicking of search button
-        self.ids.stat_name.text = "NAME: " + player_name
-        self.ids.stat_team.text = "TEAM: " + team_name
-        self.ids.stat_pos.text = "POSITION: " + pos_name
-        self.ids.stat_pts.text = "PPG: " + pts_name
-        self.ids.stat_fg.text = "FG %: " + fg_name
+        self.ids.stat_name.text = "NAME:\n" + player_name
+        self.ids.stat_team.text = "TEAM:\n" + team_name
+        self.ids.stat_pos.text = "POSITION:\n" + pos_name
+        self.ids.stat_pts.text = "PPG:\n" + pts_name
+        self.ids.stat_fg.text = "FG %:\n" + fg_name
     def clear_roster(self):#CLEAR ROSTER
         self.ids.player1.text = "PLAYER 1"
         self.ids.player2.text = "PLAYER 2"
@@ -89,27 +91,39 @@ class SecondWindow(Screen):
             if roster_length == 4:
                 self.ids.player4.text  = player_name
     def anal_roster(self):
-        if len(myroster) < 5:
+        if len(myroster) < 5:#will only allow roster analysis with a full roster (5)
             return
-        p1 = myroster[0]
-        p2 = myroster[1]
-        p3 = myroster[2]
-        p4 = myroster[3]
-        p5 = myroster[4]
+        p1 = myroster[0]#these lines reference the players stored in myroster list
+        p2 = myroster[1]#    |
+        p3 = myroster[2]#   |
+        p4 = myroster[3]#  | 
+        p5 = myroster[4]# V
         query = pd.read_sql_query("select * from t20_21_pergame WHERE \"Player\" = '"+p1+"' OR \"Player\" = '"+p2+"' OR \"Player\" = '"+p3+"' OR \"Player\" ='"+p4+"' OR \"Player\" ='"+p5+"';", conn)
         show(query)
-class WindowManager(ScreenManager):
-    pass
+    #THIS HANDLES ALL BUTTON ANIMATION
+    def animate_it(self, widget, *args):
+        #this defines our button animation
+        animate = Animation(duration=.1, opacity=.5)
+        animate += Animation(duration=.1, opacity=1)
+        #starting animation
+        animate.start(widget)
 
-kv = Builder.load_file('my.kv')
+
+class WindowManager(ScreenManager):
+    pass# no necessary fns for first screen
+
+kv = Builder.load_file('my.kv')# had to reduce this to a variable to return in myAPP
 
 #MAIN CLASS
 #my.kv file used for MyApp
 class MyApp(App):
+
     def build(self):
+        #sets title bar
+        self.title = 'KOI-B ANALYTICS'
+        #sets application background color
         Window.clearcolor = (40/255.0,44/255.0,52/255.0,1)
-        Window.maximize()
-        return kv #kv references our my.kv file as see above
+        return kv #kv references our my.kv file as see above-> line 118
 
 if __name__ == '__main__':
     MyApp().run()
